339aa0a834c03ec726b66bfb0a48610e0fa6151a
Jung Yoon <jungyoon@utexas.edu>
2015-06-23 20:36:49 -0500

    11. Write unit tests in TestNetflix.py that test corner cases and failure cases until you have an average of 3 tests for each function, confirm the expected failures, and add, commit, and push to the private code repo.

---
+++ Netflix.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+
+# -------
+# Netflix.py
+# -------
+
+from functools import reduce
+from math      import sqrt
+from numpy     import mean, sqrt, square, subtract
+import json
+
+# ------------
+# netflix_predict
+# ------------
+
+def netflix_predict () :
+    """
+    returns predict 
+    """
+
+    return 1
+
+# ------------
+# netflix_rmse
+# ------------
+
+def netflix_rmse (a, p) :
+    """
+    i the beginning of the range, inclusive
+    j the end       of the range, inclusive
+    return the max cycle length of the range [i, j]
+    """
+    return sqrt(mean(square(subtract(a, p))))
+
+# -------------
+# netflix_print
+# -------------
+
+def netflix_print (w, i) :
+    """
+    w a writer
+    j the movie_id or the customer_id
+    """
+    w.write(str(i) + "\n")
+
+# -------------
+# netflix_print_rmse
+# -------------
+
+def netflix_print_rmse (w, j) :
+    """
+    w a writer
+    j the movie_id or the customer_id
+    """
+    w.write("RMSE: " + "{0:.2f}".format(j) +  "\n")
+
+# -------------
+# netflix_solve
+# -------------
+
+def netflix_solve (r, w) :
+    """
+    r a reader
+    w a writer
+    """
+    
+    # open cache files from hard coded address
+    """
+    with open('/u/thunt/cs373-netflix-tests/ddg625-ActualCache.json', 'r') as f:
+        original_rating = json.load(f)
+    with open('/u/thunt/cs373-netflix-tests/irvin-user_avg.json', 'r') as i:
+        avg_user_rating = json.load(i)
+    with open('/u/thunt/cs373-netflix-tests/irvin-movie_avg.json', 'r') as g:
+        avg_movie_rating = json.load(g)
+    """   
+ 
+    # parses input movie by movie and places into temp_list
+    # where first value is movie_id and the rest are customer_id's
+    
+    temp_list = []
+    actual_predictions = []
+    calculated_predictions = []
+    count = 0
+
+    for s in r :
+        if s[-2] == ':' :
+            if count > 0 :
+                # handle old list --> make predictions 
+                iter_temp = iter(temp_list)
+                movie_id = next(iter_temp)
+                netflix_print(w, movie_id)
+                for q in iter_temp :
+                    #prediction = netflix_predict(movie_id, user_id, av_movie_rating, av_user_rating)
+                    #netflix_print(w, prediction)
+                    #calculated_predictions.append(prediciton)
+                    actual_predictions.append(0)
+                temp_list.clear()
+                temp_list.append(s[:-1])
+            else :
+                # setup new list
+                temp_list.clear()
+                temp_list.append(s[:-1])
+                count += 1
+        else :
+            temp_list.append(s)     
+    # handle last list iteration
+    iter_temp = iter(temp_list)
+    movie_id = next(iter_temp)
+    netflix_print(w, movie_id)
+    for q in iter_temp :
+        #prediction = netflix_predict(movie_id, user_id, av_movie_rating, av_user_rating)
+        #netflix_print(w, prediction)
+        #calculated_predictions.append(prediciton)
+        actual_predictions.append(0)
+
+    #rmse = netflix_rmse(actual_predictions, calculated_predictions)
+    #w.write(len(calculated_predictions))
+   
---
+++ TestNetflix.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+
+# https://docs.python.org/3.4/reference/simple_stmts.html#grammar-token-assert_stmt
+
+# -------
+# imports
+# -------
+
+from io       import StringIO
+from unittest import main, TestCase
+
+from Netflix import netflix_predict, netflix_rmse, netflix_print, netflix_print_rmse, netflix_solve
+
+# -----------
+# TestNetflix
+# -----------
+
+class TestNetflix (TestCase) :
+    # ----
+    # predict
+    # ----
+
+    def test_predict_1 (self) :
+        i = netflix_predict(5, 5)
+        self.assertEqual(i,  5)
+
+    def test_predict_2 (self) :
+        i = netflix_predict(1,1)
+        self.assertEqual(i,  1)
+   
+
+    # ----
+    # rmse
+    # ----
+   
+    def test_rmse_1 (self) :
+        v = netflix_rmse(1, 1)
+        self.assertEqual(v, 0)
+
+    def test_rmse_2 (self) :
+        v = netflix_rmse((1,2,3), (2,3,4))
+        self.assertEqual(v, 1)
+
+    def test_rmse_3 (self) :
+        v = netflix_rmse((2,3,4), (4,5,6))
+        self.assertEqual(v, 2)
+
+    def test_rmse_4 (self) :
+        v = netflix_rmse((2,3,4), (4,3,2))
+        self.assertEqual(v, 1.632993161855452)
+
+    def test_rmse_5 (self) :
+        v = netflix_rmse((1,2,3), (7,8,9))
+        self.assertEqual(v, 6)
+
+    # -----
+    # print
+    # -----
+
+    def test_print_1 (self) :
+        w = StringIO()
+        netflix_print(w, 3423)
+        self.assertEqual(w.getvalue(), "3423\n")
+
+    def test_print_2 (self) :
+        w = StringIO()
+        netflix_print(w, 92838)
+        self.assertEqual(w.getvalue(), "92838\n")
+
+    def test_print_3 (self) :
+        w = StringIO()
+        netflix_print(w, 11134234)
+        self.assertEqual(w.getvalue(), "11134234\n")
+
+    def test_print_4 (self) :
+        w = StringIO()
+        netflix_print(w, 1)
+        self.assertEqual(w.getvalue(), "1\n")
+
+    def test_print_5 (self) :
+        w = StringIO()
+        netflix_print(w, 9293277)
+        self.assertEqual(w.getvalue(), "9293277\n")
+
+    # -----
+    # print rmse
+    # -----
+
+    def test_print_rmse_1 (self) :
+        w = StringIO()
+        netflix_print_rmse(w, .951)
+        self.assertEqual(w.getvalue(), "RMSE: 0.95\n")
+
+    def test_print_rmse_2 (self) :
+        w = StringIO()
+        netflix_print_rmse(w, 92)
+        self.assertEqual(w.getvalue(), "RMSE: 92.00\n")
+
+    def test_print_rmse_3 (self) :
+        w = StringIO()
+        netflix_print_rmse(w, 1.777)
+        self.assertEqual(w.getvalue(), "RMSE: 1.78\n")
+
+    def test_print_rmse_4 (self) :
+        w = StringIO()
+        netflix_print_rmse(w, 1.772)
+        self.assertEqual(w.getvalue(), "RMSE: 1.77\n")
+
+    def test_print_rmse_5 (self) :
+        w = StringIO()
+        netflix_print_rmse(w, 192.3)
+        self.assertEqual(w.getvalue(), "RMSE: 192.30\n")
+
+    # -----
+    # solve
+    # -----
+
+    def test_solve_1 (self) :
+        r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
+        w = StringIO()
+        netflix_solve(r, w)
+        self.assertEqual(w.getvalue(), "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
+
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__" :
+    main()
+
+"""
+% coverage3 run --branch TestCollatz.py >  TestCollatz.out 2>&1
+
+
+
+% coverage3 report -m                   >> TestCollatz.out
+
+
+
+% cat TestCollatz.out
+.......
+----------------------------------------------------------------------
+Ran 7 tests in 0.001s
+
+OK
+Name          Stmts   Miss Branch BrMiss  Cover   Missing
+---------------------------------------------------------
+Collatz          18      0      6      0   100%
+TestCollatz      33      1      2      1    94%   79
+---------------------------------------------------------
+TOTAL            51      1      8      1    97%
+"""
+
